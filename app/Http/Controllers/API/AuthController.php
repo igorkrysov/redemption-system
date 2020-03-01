d50b424d6748938423992e4bef95c0bf3bd99298
Igor Krysov <igorkrysov@mail.ru>
2020-03-01 23:57:46 +0600

    auth features

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) 
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json(['status' => false, 'errors' => $validator->messages()]);
+        }
+
+        if (Auth::atempt(['email' => $request->email, 'password' => $request->password])) {
+            $token = Auth::User()->createToken('auth')->accessToken;
+
+            return response()->json(['status' => true, 'token' => $token]);
+        }
+
+        return response()->json(['status' => false, 'meesages' => ['Login or password are incorrect']]);
+    }
+    public function logout(Request $request) 
+    {
+        Auth::logout();
+
+        return response()->json(['status' => true]);
+    }
+}
