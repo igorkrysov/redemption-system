401b0f2ab8c1a724df085b10b0781fc32cbbae34
Igor Krysov <igorkrysov@mail.ru>
2020-03-02 16:12:04 +0600

    add package vue-json-pretty

--- public/js/app.js
+++ public/js/app.js
@@ -50626,8 +50626,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/cardoc/test-task/blog6/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/cardoc/test-task/blog6/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/test-task/redemption-system/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/test-task/redemption-system/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
